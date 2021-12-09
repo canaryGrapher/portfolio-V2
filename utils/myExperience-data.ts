87d02d3b8bd703ac90f2fcfe12a3b33637d70fb5
Yash Aryan <yasharyan307@outlook.com>
2021-12-10 01:43:14 +0530

    Added logos for experience section

--- utils/myExperience-data.ts
+++ utils/myExperience-data.ts
@@ -9,7 +9,7 @@ const myExperience: IExperience[] = [
         role: "Trainee",
         timeline: 'October 2021 - present',
         description: "Working on an Open-source project",
-        image: "https://ik.imagekit.io/canarygrapher/Portfolio/home/Experience/Formal/ioncure_skTqj0eHYZL.png?updatedAt=1636521857506https://ik.imagekit.io/canarygrapher/Portfolio/home/Experience/Formal/nonceblox_oYjryRGcf.png?updatedAt=1636521857791"
+        image: "https://ik.imagekit.io/canarygrapher/Portfolio/home/Experience/Formal/IISC_MZbuZsCN4Wb.jpg"
     },
     {
         name: "Nonceblox Private Limited",
@@ -41,15 +41,7 @@ const myExperience: IExperience[] = [
         role: "Intern",
         timeline: 'January 2019 - present',
         description: "Helped in various WebDev projects, ideation process, conducting Hackathons, and managing servers for test projects.",
-        image: "https://ik.imagekit.io/canarygrapher/Portfolio/home/Experience/Formal/ioncure_skTqj0eHYZL.png?updatedAt=1636521857506https://ik.imagekit.io/canarygrapher/Portfolio/home/Experience/Formal/nonceblox_oYjryRGcf.png?updatedAt=1636521857791"
-    },
-    {
-        name: "The ChillSpace",
-        designation: 'Frontend Web Developer',
-        role: "Freelancer",
-        timeline: 'September 2020 - November 2020',
-        description: "Involved with a team of students from the Manipal Institute of Technology who worked on the ‘Hourglass Portal’ for the Recreation Center, which was designed as a user management portal-cum-reward system. My task included reproducing the web pages from UI designs using HTML, CSS, and JavaScript, suggesting the client for possible changes in the structure of the application for better usability, and coordinating with the design team to better understand and suggest changes to the design",
-        image: "https://ik.imagekit.io/canarygrapher/Portfolio/home/Experience/Formal/ioncure_skTqj0eHYZL.png?updatedAt=1636521857506https://ik.imagekit.io/canarygrapher/Portfolio/home/Experience/Formal/nonceblox_oYjryRGcf.png?updatedAt=1636521857791"
+        image: "https://ik.imagekit.io/canarygrapher/Portfolio/home/Experience/Formal/innovation_center_kyrzpd1G2-8a.jpg"
     },
 ]
 
@@ -60,8 +52,8 @@ const informalExperience: IExperience[] = [
         designation: 'Binance Campus BUIDLer',
         role: "Volunteer",
         timeline: 'November 2021 - present',
-        description: "",
-        image: "https://ik.imagekit.io/canarygrapher/Portfolio/home/Experience/Formal/ioncure_skTqj0eHYZL.png?updatedAt=1636521857506https://ik.imagekit.io/canarygrapher/Portfolio/home/Experience/Formal/nonceblox_oYjryRGcf.png?updatedAt=1636521857791"
+        description: "Representative of the Binance Campus BUIDLer program, which is a program to help students build crypto skills and get introduced to the Binance ecosystem.",
+        image: "https://ik.imagekit.io/canarygrapher/Portfolio/home/Experience/Informal/binance_4979Rk6qk.png?updatedAt=1639079290305"
     },
     {
         name: "Codecademy Community",
@@ -69,7 +61,7 @@ const informalExperience: IExperience[] = [
         role: "Volunteer",
         timeline: 'August 2021 - present',
         description: "Organizing events and share knowledge with the Codecademy community in Manipal Institute of Technology, working on projects together and helping out fellow community members in understanding concepts.",
-        image: "https://ik.imagekit.io/canarygrapher/Portfolio/home/Experience/Formal/ioncure_skTqj0eHYZL.png?updatedAt=1636521857506https://ik.imagekit.io/canarygrapher/Portfolio/home/Experience/Formal/nonceblox_oYjryRGcf.png?updatedAt=1636521857791"
+        image: "https://ik.imagekit.io/canarygrapher/Portfolio/home/Experience/Informal/codecademy_VMHGMCur5.jpg?updatedAt=1639079305981"
     },
     {
         name: "SOCIIO ichor",
@@ -77,63 +69,63 @@ const informalExperience: IExperience[] = [
         role: "Volunteer",
         timeline: 'August 2021 - November 2021',
         description: "My tasks included understand the project and make recommendations to functionality, making apt UI designs for the project and helping with the hosting of the web application on AWS",
-        image: "https://ik.imagekit.io/canarygrapher/Portfolio/home/Experience/Formal/ioncure_skTqj0eHYZL.png?updatedAt=1636521857506https://ik.imagekit.io/canarygrapher/Portfolio/home/Experience/Formal/nonceblox_oYjryRGcf.png?updatedAt=1636521857791"
+        image: "https://ik.imagekit.io/canarygrapher/Portfolio/home/Experience/Informal/sociio_H4vwSL0bUK.jpg?updatedAt=1639079306097"
     },
     {
         name: "Manipal Information Security Team",
         designation: 'Head of Web Development',
         role: "Volunteer",
         timeline: 'July 2020 - June 2021',
-        description: "",
-        image: "https://ik.imagekit.io/canarygrapher/Portfolio/home/Experience/Formal/ioncure_skTqj0eHYZL.png?updatedAt=1636521857506https://ik.imagekit.io/canarygrapher/Portfolio/home/Experience/Formal/nonceblox_oYjryRGcf.png?updatedAt=1636521857791"
+        description: "Maintaining and updating the website of the club and also taking care of various hosting needs for the organized events. Imparting knowledge to other members of the club through webinars/meetups",
+        image: "https://ik.imagekit.io/canarygrapher/Portfolio/home/Experience/Informal/mist_mthQ_mKG3Fl.jpg?updatedAt=1639079306500"
     },
     {
         name: "Manipal Information Security Team",
         designation: 'Management Committee Member',
         role: "Volunteer",
         timeline: 'August 2019 - July 2020',
-        description: "",
-        image: "https://ik.imagekit.io/canarygrapher/Portfolio/home/Experience/Formal/ioncure_skTqj0eHYZL.png?updatedAt=1636521857506https://ik.imagekit.io/canarygrapher/Portfolio/home/Experience/Formal/nonceblox_oYjryRGcf.png?updatedAt=1636521857791"
+        description: "Managing the freshers and conducting events and workshops. Re-design and develop the website for the club, social media campaign for the club imparting knowledge to other members of the club through webinars / meetups",
+        image: "https://ik.imagekit.io/canarygrapher/Portfolio/home/Experience/Informal/mist_mthQ_mKG3Fl.jpg?updatedAt=1639079306500"
     },
     {
         name: "Manipal Information Security Team",
         designation: 'Working Committee Member',
         role: "Volunteer",
         timeline: 'September 2018 - August 2019',
-        description: "",
-        image: "https://ik.imagekit.io/canarygrapher/Portfolio/home/Experience/Formal/ioncure_skTqj0eHYZL.png?updatedAt=1636521857506https://ik.imagekit.io/canarygrapher/Portfolio/home/Experience/Formal/nonceblox_oYjryRGcf.png?updatedAt=1636521857791"
+        description: "Provided support in hosting and managing events for the senior members and actively take part in community events",
+        image: "https://ik.imagekit.io/canarygrapher/Portfolio/home/Experience/Informal/mist_mthQ_mKG3Fl.jpg?updatedAt=1639079306500"
     },
     {
         name: "TechTatva, MIT Manipal",
         designation: 'Category Head',
         role: "Volunteer",
         timeline: 'October 2020',
-        description: "",
-        image: "https://ik.imagekit.io/canarygrapher/Portfolio/home/Experience/Formal/ioncure_skTqj0eHYZL.png?updatedAt=1636521857506https://ik.imagekit.io/canarygrapher/Portfolio/home/Experience/Formal/nonceblox_oYjryRGcf.png?updatedAt=1636521857791"
+        description: "One of the three Category Heads for Manipal Institute of Technology's tech-fest TechTatva'20, Turing, a CyberSec related category. Responsibilities included overseeing Ctrl+C event, a WebDev event where the participants were challenged to create a rigged website replicating the one provided to them and extract data from the account upon successful login, oversee the event and act immediately in case of server crashes, or other discrepancies and responsible for maintaining the deployment of the other events under this category on the server.",
+        image: "https://ik.imagekit.io/canarygrapher/Portfolio/home/Experience/Informal/techtatva20_i6M0po1Fz41.png?updatedAt=1639080260741"
     },
     {
         name: "TechTatva, MIT Manipal",
-        designation: '',
+        designation: 'Event Head',
         role: "Volunteer",
         timeline: 'October 2020',
-        description: "",
-        image: "https://ik.imagekit.io/canarygrapher/Portfolio/home/Experience/Formal/ioncure_skTqj0eHYZL.png?updatedAt=1636521857506https://ik.imagekit.io/canarygrapher/Portfolio/home/Experience/Formal/nonceblox_oYjryRGcf.png?updatedAt=1636521857791"
+        description: "Event Head for Manipal Institute of Technology's tech-fest TechTatva'20, for Turing, a CyberSec-related category. Responsibilitie included overseeing the Ctrl+C event, a WebDev event where the participants had to create a rigged website replicating the one provided to them and extract data from the account upon successful login.",
+        image: "https://ik.imagekit.io/canarygrapher/Portfolio/home/Experience/Informal/techtatva20_i6M0po1Fz41.png?updatedAt=1639080260741"
     },
     {
         name: "TechTatva, MIT Manipal",
-        designation: '',
+        designation: 'Event Organizer',
         role: "Volunteer",
-        timeline: 'September 20 - August 20',
-        description: "",
-        image: "https://ik.imagekit.io/canarygrapher/Portfolio/home/Experience/Formal/ioncure_skTqj0eHYZL.png?updatedAt=1636521857506https://ik.imagekit.io/canarygrapher/Portfolio/home/Experience/Formal/nonceblox_oYjryRGcf.png?updatedAt=1636521857791"
+        timeline: 'October 19',
+        description: "Assigned with the task of creating web vulnerability questions for the CTF event and responsible for deployment of a part of the questions",
+        image: "https://ik.imagekit.io/canarygrapher/Portfolio/home/Experience/Informal/techtatva19_uELDAvOHtCy.png?updatedAt=1639079290299"
     },
     {
         name: "TechTatva, MIT Manipal",
-        designation: '',
+        designation: 'Student Volunteer',
         role: "Volunteer",
-        timeline: 'September 20 - August 20',
-        description: "",
-        image: "https://ik.imagekit.io/canarygrapher/Portfolio/home/Experience/Formal/ioncure_skTqj0eHYZL.png?updatedAt=1636521857506https://ik.imagekit.io/canarygrapher/Portfolio/home/Experience/Formal/nonceblox_oYjryRGcf.png?updatedAt=1636521857791"
+        timeline: 'October 18',
+        description: "Part of the System Admin and Web Development team for managing the website of the tech-fest of Manipal Institute of Technology",
+        image: "https://ik.imagekit.io/canarygrapher/Portfolio/home/Experience/Informal/techtatva18_vJZZ9GzbC.jpg?updatedAt=1639079290299"
     },
 ]
 
