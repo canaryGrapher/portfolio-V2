661b2069c38fc97f843fc6f97323c04c34253fc9
Yash Aryan <yasharyan307@outlook.com>
2022-04-12 15:42:55 +0530

    Added Google analytics tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import Head from "next/head";
-import Script from "next/script";
 import { useRouter } from "next/router";
 import { AppProps } from "next/dist/shared/lib/router/router";
 
@@ -22,7 +21,9 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   const router = useRouter();
   useEffect(() => {
     const handleRouteChange = (url: string) => {
-      gtag.pageview(url);
+      window.gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+        page_path: url,
+      });
     };
     router.events.on("routeChangeComplete", handleRouteChange);
     return () => {
@@ -31,24 +32,24 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   }, [router.events]);
   return (
     <>
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
+      <Head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
               page_path: window.location.pathname,
             });
           `,
-        }}
-      />
+          }}
+        />
+      </Head>
       <div className="overflow-x-hidden">
         <Head>
           <meta charSet="utf-8" />
