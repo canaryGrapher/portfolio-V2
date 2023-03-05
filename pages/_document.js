a897097c6c5d5269f32f3af314b4898941c16abf
Yash Aryan <yasharyan307@outlook.com>
2023-03-05 03:16:58 -0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,19 +5,18 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-W89RKGX');</script>
-<!-- End Google Tag Manager -->
+          <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-W89RKGX');
+          </script>
         </Head>
         <body>
-      <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W89RKGX"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
+            <noscript>
+              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W89RKGX"
+                height="0" width="0" style="display:none;visibility:hidden"></iframe>
+            </noscript>
           <Main />
           <NextScript />
         </body>
